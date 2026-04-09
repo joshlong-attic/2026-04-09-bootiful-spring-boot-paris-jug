74a85ce5e469dd5f08f54a45966677681c3819a5
Josh Long <joshua.long@broadcom.com>
2026-04-09 22:05:11 +0200

    aot

--- adoptions/src/main/java/com/example/adoptions/AdoptionsApplication.java
+++ adoptions/src/main/java/com/example/adoptions/AdoptionsApplication.java
@@ -1,21 +1,40 @@
 package com.example.adoptions;
 
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.beans.factory.aot.BeanFactoryInitializationAotContribution;
+import org.springframework.beans.factory.aot.BeanFactoryInitializationAotProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
 import org.springframework.http.MediaType;
 import org.springframework.resilience.annotation.EnableResilientMethods;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestClient;
+import tools.jackson.databind.json.JsonMapper;
 
 import javax.sql.DataSource;
+import java.io.Serializable;
+import java.nio.charset.Charset;
 import java.security.Principal;
+import java.util.HashSet;
 import java.util.Map;
 
 @EnableResilientMethods
@@ -26,9 +45,59 @@ public class AdoptionsApplication {
         SpringApplication.run(AdoptionsApplication.class, args);
     }
 
+    @Bean
+    JdbcPostgresDialect jdbcPostgresDialect() {
+        return JdbcPostgresDialect.INSTANCE;
+    }
+
+    @Bean
+    MyBeanFactoryInitializationAotProcessor myBeanFactoryInitializationAotProcessor() {
+        return new MyBeanFactoryInitializationAotProcessor();
+    }
 
 }
 
+// 10%
+class MyBeanFactoryInitializationAotProcessor implements BeanFactoryInitializationAotProcessor {
+
+    @Override
+    public @Nullable BeanFactoryInitializationAotContribution processAheadOfTime(
+            ConfigurableListableBeanFactory beanFactory) {
+
+        var serializable = new HashSet<Class<?>>();
+        for (var beanName : beanFactory.getBeanDefinitionNames()) {
+            IO.println(beanName);
+            var type = beanFactory.getType(beanName);
+            IO.println(type.getName());
+
+            if (Serializable.class.isAssignableFrom(type)) {
+                serializable.add(type);
+                IO.println("is " + type.getName() + " serializable?");
+            }
+        }
+
+        return (generationContext, _) -> {
+
+            for (var s : serializable) {
+                var hints = generationContext.getRuntimeHints();
+                hints.serialization().registerType(TypeReference.of(s));
+            }
+        };
+    }
+}
+
+
+@Component
+class ShoppingCart implements Serializable {
+    //
+}
+
+// 0. ingest (xml, component scanning, java config, beanRegistrar)
+// 1. BeanDefinitions
+// 2. beans
+
+
+@ImportRuntimeHints(SecurityConfiguration.ParisJugRuntimeHintsRegistrar.class)
 @Configuration
 class SecurityConfiguration {
 
@@ -53,6 +122,36 @@ class SecurityConfiguration {
         var u = new JdbcUserDetailsManager(dataSource);
         u.setEnableUpdatePassword(true);
         return u;
+    }
+
+    // 90%
+    static class ParisJugRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
+            hints.resources().registerResource(RESOURCE);
+            hints.reflection().registerType(Cat.class, MemberCategory.values());
+        }
+    }
+
+    private static final ClassPathResource RESOURCE = new ClassPathResource("/message");
+
+    @Bean
+    ApplicationRunner jacksonRunner(JsonMapper jsonMapper) {
+        return a -> {
+            var garfield = new Cat(1, "Garfield");
+            var obj = jsonMapper.writeValueAsString(garfield);
+            IO.println(obj);
+        };
+    }
+
+    record Cat(int id, String name) {
+    }
+
+    @Bean
+    ApplicationRunner resourceRunner() {
+        return _ -> IO.println(
+                RESOURCE.getContentAsString(Charset.defaultCharset()));
     }
 
 }
@@ -92,6 +191,3 @@ class VirtualThreadsController {
     }
 
 }
-
-
-
--- adoptions/src/main/java/com/example/adoptions/dogs/DogRepository.java
+++ adoptions/src/main/java/com/example/adoptions/dogs/DogRepository.java
@@ -2,5 +2,9 @@ package com.example.adoptions.dogs;
 
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Collection;
+
 interface DogRepository extends ListCrudRepository<Dog, Integer> {
+
+    Collection <Dog> findByName (String name);
 }
